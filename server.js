5dece37f4ccd2180dbe8df015dd4bb1f36fd79db
Lisandra Cruz <lisansouza@gmail.com>
2022-05-06 15:10:26 -0300

    First route configuration to return status and data in the body

--- server.js
+++ server.js
@@ -5,7 +5,14 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .json({status: {success: true},data: {welcome: 'hello', farewell: 'goodbye'}})
+})
+
+const PORT = process.env.PORT || 8000;
 
 app.listen(
   PORT,
