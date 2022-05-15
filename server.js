3d7951629f58d25493b726d23d3d702e461d4055
Lisandra Cruz <lisansouza@gmail.com>
2022-05-15 14:23:33 -0300

    Connection with mongo db and adding mongan lib to log http requests

--- server.js
+++ server.js
@@ -1,28 +1,41 @@
+const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-const express = require("express");
-const app = express();
+const colors = require('colors');
+const connectDB = require('./config/db');
 
-const logger = require('./middleware/logger')
-app.use(logger);
+// Load env vars
+dotenv.config({ path: "./config/config.env" });
+
+//Connect to database
+connectDB();
+
+//Route files
+const bootcamps = require('./routes/bootcamps');
+
+const app = express();
 
 //Dev logging middleware
 if(process.env.NODE_ENV === 'development'){
 	app.use(morgan('dev'))
 }
 
-// Load env vars
-dotenv.config({ path: "./config/config.env" });
+const logger = require('./middleware/logger')
+app.use(logger);
 
-//Route files
-const bootcamps = require('./routes/bootcamps');
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
-
 const PORT = process.env.PORT || 8000;
-app.listen(
+const server = app.listen(
   PORT,
-	() => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+	() => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+	console.log(`Error: ${err.message}`.red);
+	// Close server and exit precess
+	server.close(() => process.exit(1));
+})
